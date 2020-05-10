2ddded9dbcd315d30e263db91e0fe5a7b61a7100
korhan <korhanyuzbas@gmail.com>
2020-05-10 15:20:33 +0300

    email verification threshold

--- internal/configs/constants.go
+++ internal/configs/constants.go
@@ -1,8 +1,9 @@
 package configs
 
 const (
-	ValidatorKey  = "ValidatorKey"
-	TranslatorKey = "TranslatorKey"
+	ValidatorKey                     = "ValidatorKey"
+	TranslatorKey                    = "TranslatorKey"
+	EmailVerificationExpiryThreshold = 3
 )
 
 var JwtKey = "jwt-key"
